ec793fa27ad34816400d6a8d6c90b9ecbfb37e65
sergiopoliveira <sergiopoliveira@outlook.com>
2018-05-16 12:15:54 +0100

    Reentrant Locks

--- src/com/sergio/Main.java
+++ src/com/sergio/Main.java
@@ -3,6 +3,7 @@ package com.sergio;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static com.sergio.Main.EOF;
 
@@ -11,9 +12,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		List<String> buffer = new ArrayList<String>();
-		MyProducer producer = new MyProducer(buffer, "YELLOW");
-		MyConsumer consumer1 = new MyConsumer(buffer, "PURPLE");
-		MyConsumer consumer2 = new MyConsumer(buffer, "CYAN");
+		ReentrantLock bufferLock = new ReentrantLock();
+		MyProducer producer = new MyProducer(buffer, "YELLOW", bufferLock);
+		MyConsumer consumer1 = new MyConsumer(buffer, "PURPLE", bufferLock);
+		MyConsumer consumer2 = new MyConsumer(buffer, "CYAN", bufferLock);
 
 		new Thread(producer).start();
 		new Thread(consumer1).start();
@@ -25,10 +27,12 @@ class MyProducer implements Runnable {
 
 	private List<String> buffer;
 	private String color;
+	private ReentrantLock bufferLock;
 
-	public MyProducer(List<String> buffer, String color) {
+	public MyProducer(List<String> buffer, String color, ReentrantLock bufferLock) {
 		this.buffer = buffer;
 		this.color = color;
+		this.bufferLock = bufferLock;
 	}
 
 	@Override
@@ -39,10 +43,9 @@ class MyProducer implements Runnable {
 		for (String num : nums) {
 			try {
 				System.out.println("Adding... " + num);
-				synchronized(buffer) {
+				bufferLock.lock();
 				buffer.add(num);
-				}
-
+				bufferLock.unlock();
 				Thread.sleep(random.nextInt(1000));
 			} catch (InterruptedException e) {
 				System.out.println("Producer was interrupted");
@@ -50,35 +53,40 @@ class MyProducer implements Runnable {
 		}
 
 		System.out.println("Adding EOF and exiting...");
-		synchronized(buffer) {
+		bufferLock.lock();
 		buffer.add("EOF");
-		}
+		bufferLock.unlock();
 	}
 }
 
 class MyConsumer implements Runnable {
 	private List<String> buffer;
 	private String color;
+	private ReentrantLock bufferLock;
 
-	public MyConsumer(List<String> buffer, String color) {
+	public MyConsumer(List<String> buffer, String color, ReentrantLock bufferLock) {
 		this.buffer = buffer;
 		this.color = color;
+		this.bufferLock = bufferLock;
 	}
 
 	@Override
 	public void run() {
+
 		while (true) {
-			synchronized(buffer) {
+			bufferLock.lock();
 			if (buffer.isEmpty()) {
+				bufferLock.unlock();
 				continue;
-				}
+			}
 			if (buffer.get(0).equals(EOF)) {
 				System.out.println("Exiting");
+				bufferLock.unlock();
 				break;
 			} else {
 				System.out.println("Removed " + buffer.remove(0));
 			}
-		}
+			bufferLock.unlock();
 		}
 	}
-	}
+}
